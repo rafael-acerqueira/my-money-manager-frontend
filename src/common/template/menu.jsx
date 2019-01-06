cfd29f4b44f6b0bca590b272bbcb1f76498e8bb9
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-06 18:29:22 -0300

    Create Menu Components and Sidebar

---
+++ src/common/template/menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
+
+export default props => (
+  <ul className='sidebar-menu'>
+    <MenuItem path='#/' icon='dashboard' label='Dashboard'/>
+    <MenuTree label='Cadastro' icon='edit'>
+      <MenuItem path='#billingCycles' label='Ciclos de pagamento' icon='usd' />
+    </MenuTree>
+  </ul>
+)
