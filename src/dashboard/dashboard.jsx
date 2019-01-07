852cc398e1ca92bae81f32565bc91334cb68043e
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-07 11:08:02 -0300

    Connect Dashboard Component with backend

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -6,17 +7,36 @@ import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 
+const BASE_URL = 'http://localhost:3003/api'
+
 class Dashboard extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = { credit: 0, debt: 0 }
+  }
+
+
+  componentDidMount() {
+    axios.get(`${BASE_URL}/billingCycles/summary`)
+          .then( resp => this.setState(resp.data))
+  }
+
   render() {
+
+    const { credit, debt } = this.state
     return(
       <div>
         <ContentHeader title='Dashboard' subtitle='Versão 1.0' />
         <Content>
           <Row>
-            <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos'/>
-            <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos'/>
-            <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor Consolidado'/>
+            <ValueBox cols='12 4' color='green' icon='bank' 
+                      value={`R$ ${credit}`} text='Total de Créditos'/>
+            <ValueBox cols='12 4' color='red' icon='credit-card' 
+                      value={`R$ ${debt}`} text='Total de Débitos'/>
+            <ValueBox cols='12 4' color='blue' icon='money' 
+                      value={`R$ ${credit - debt}`} text='Valor Consolidado'/>
           </Row>
         </Content>
       </div>
