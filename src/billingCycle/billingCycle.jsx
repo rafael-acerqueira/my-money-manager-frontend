da35d89d0cf5d1ae097bcb7740296132ff057cbe
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-08 10:02:45 -0300

    Make tabList selected on first load

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,8 +9,15 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
-export default class BillingCycle extends Component{
+class BillingCycle extends Component{
+
+
+componentWillMount() {
+  this.props.selectTab('tabList')
+}
+
   render() {
     return(
       <div>
@@ -35,3 +44,6 @@ export default class BillingCycle extends Component{
     )
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
