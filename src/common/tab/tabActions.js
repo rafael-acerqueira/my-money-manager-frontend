275020957fb0b2c104c79b2a4d6657455ded33ca
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-08 11:02:40 -0300

    Create action and reducer to set which tab will be showed

--- src/common/tab/tabActions.js
+++ src/common/tab/tabActions.js
@@ -4,3 +4,13 @@ export function selectTab(tabId) {
     payload: tabId
   }
 }
+
+export function showTabs(...tabIds) {
+  const tabsToShow = {}
+  tabIds.forEach( id => tabsToShow[id] = true)
+
+  return {
+    type: 'TAB_SHOWED',
+    payload: tabsToShow
+  }
+}
