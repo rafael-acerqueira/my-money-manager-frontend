55a4b00a76470a1eec637b3bf55bb876f4f3881b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-08 18:26:50 -0300

    Create reducer and action to manage the list of billingCycles

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux'
 
 import tabReducer from '../common/tab/tabReducer'
+import billingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
-  tab: tabReducer
+  tab: tabReducer,
+  billingCycle: billingCycleReducer
+
 })
 
 export default rootReducer
