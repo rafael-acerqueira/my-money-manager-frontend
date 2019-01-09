d58520b7ce3bd2d5dcd0c2b3c3cedcd1d3deb374
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-09 09:37:05 -0300

    Create a custom input and label component to use in redux form

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import LabelAndInput from '../common/form/labelAndInput'
+
 class BillingCycleForm extends Component {
   render() {
 
@@ -9,9 +11,9 @@ class BillingCycleForm extends Component {
     return(
       <form role='form' onSubmit={handleSubmit}>
         <div className='box-body'>
-          <Field name='name' component='input' />
-          <Field name='month' component='input' />
-          <Field name='year' component='input' />
+          <Field name='name' component={LabelAndInput} />
+          <Field name='month' component={LabelAndInput} />
+          <Field name='year' component={LabelAndInput} />
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
