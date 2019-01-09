0eaf3e70915a55fe9ae0d87227a1ef337e69a239
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-09 08:37:14 -0300

    Configure react redux toastr

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from  'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import tabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from '../billingCycle/billingCycleReducer'
@@ -7,7 +8,8 @@ import billingCycleReducer from '../billingCycle/billingCycleReducer'
 const rootReducer = combineReducers({
   tab: tabReducer,
   billingCycle: billingCycleReducer,
-  form: formReducer
+  form: formReducer,
+  toastr: toastrReducer
 
 })
 
