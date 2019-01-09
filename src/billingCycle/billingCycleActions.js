fb82129744b5f12efc5c8d6d67612469bc146c16
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-09 16:01:05 -0300

    Creates initial form state

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -4,6 +4,7 @@ import { reset as resetForm, initialize } from 'redux-form'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 export function getList() {
   const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -39,5 +40,15 @@ export function showUpdate(billingCycle) {
     showTabs('tabUpdate'),
     selectTab('tabUpdate'),
     initialize('billingCycleForm', billingCycle)
+  ]
+}
+
+export function init() {
+  return [
+    showTabs('tabList', 'tabCreate'),
+    selectTab('tabList'),
+    getList(),
+    initialize('billingCycleForm', INITIAL_VALUES)
+
   ]
 }
--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { reduxForm, Field } from 'redux-form'
 
+import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
@@ -20,10 +23,15 @@ class BillingCycleForm extends Component {
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
+          <button type='button' className='btn btn-default' 
+                  onClick={this.props.init}>Cancelar</button>
         </div>
       </form>
     )
   }
 }
 
-export default reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
+
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
