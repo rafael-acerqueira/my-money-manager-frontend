efd5ec66a1a6e803019137e9c1c5d934426db8e7
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-09 07:40:16 -0300

    Add redux-form in rootReducers

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from  'redux-form'
 
 import tabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
   tab: tabReducer,
-  billingCycle: billingCycleReducer
+  billingCycle: billingCycleReducer,
+  form: formReducer
 
 })
 
