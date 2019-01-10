4ccdd3b89ce3219933ddbf89b408b6f2d1c66ee8
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-10 17:33:04 -0300

    Refactor creditList to itemList

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -5,7 +5,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
   render() {
@@ -24,7 +24,8 @@ class BillingCycleForm extends Component {
           <Field name='year' component={LabelAndInput} type='number'
                   label='Ano' cols='12 4' placeholder='Informe o ano' 
                   readOnly={readOnly} />
-          <CreditList cols='12 6' readOnly={readOnly} list={credits}/>        
+          <ItemList cols='12 6' readOnly={readOnly} list={credits}
+                    field='credits' legend='Créditos'/>        
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
