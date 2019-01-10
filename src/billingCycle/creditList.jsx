6179f7972731c2cf54bddbba2f13cbb8290a6b9a
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-10 17:18:29 -0300

    Add new element at credit list

--- src/billingCycle/creditList.jsx
+++ src/billingCycle/creditList.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Field, arrayInsert } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
 
 class CreditList extends Component {
 
-  renderRows() {
+  add(index, item = {}) {
+    if(!this.props.readOnly){
+      this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+    }
+  }
 
+  renderRows() {
     const list = this.props.list || []
     return list.map((item, index) => (
       <tr key={item._id}>
@@ -24,11 +31,15 @@ class CreditList extends Component {
                  readOnly={this.props.readOnly}
           />
         </td>
-        <td></td>        
+        <td>
+          <button type='button' className='btn btn-success'
+                  onClick={() => this.add(index + 1)}>
+            <i className='fa fa-plus'></i>
+          </button>
+        </td>        
       </tr>
     ))    
   }
-
 
   render() {
     return(
@@ -40,7 +51,7 @@ class CreditList extends Component {
               <tr>
                 <th>Nome</th>
                 <th>Valor</th>
-                <th>Ações</th>
+                <th className='table-actions'>Ações</th>
               </tr>            
             </thead>
             <tbody>
@@ -53,4 +64,6 @@ class CreditList extends Component {
   }
 }
 
-export default CreditList
+const mapDispatchToProps = dispach => bindActionCreators({arrayInsert}, dispach)
+
+export default connect(null, mapDispatchToProps)(CreditList)
