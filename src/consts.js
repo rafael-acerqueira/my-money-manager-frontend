fbae660690eeb1c8e7ea4ccead60da58e8f10c87
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-14 20:40:29 -0300

    Add dotenv

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,4 @@
-const domain = process.env.DOMAIN ? process.env.DOMAIN : 'localhost:3003'
 export default { 
-  API_URL: `http://${domain}/api`, 
-  OAPI_URL: `http://${domain}/oapi`
+  API_URL: `http://${process.env.REACT_APP_DOMAIN}/api`, 
+  OAPI_URL: `http://${process.env.REACT_APP_DOMAIN}/oapi`
 }
